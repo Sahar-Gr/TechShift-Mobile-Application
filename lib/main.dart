de7207959d5c2f507301a0db9d14d169927aeb36
ihebmeftah <ihebmeftah666@gmail.com>
2024-06-27 22:39:26 +0100

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,23 @@
-import 'package:flutter/material.dart';
+import 'package:applicationpfe/app/core/themes/appthemes.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:applicationpfe/Login.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyDxX9FBxjAtZJt-l6OO2bKXy7buYl66WE8",
-          appId: '1090766392131:android:0c75338b55624dd461fdf6',
-          messagingSenderId: '1090766392131',
-          projectId: 'ennakl-f27a2'));
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    GetMaterialApp(
+      title: "Ennakl",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark().copyWith(
-        primaryColor: Colors.teal,
-        scaffoldBackgroundColor: Colors.black,
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: Colors.white),
-          bodyMedium: TextStyle(color: Colors.white),
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.grey,
-          border: OutlineInputBorder(),
-          hintStyle: TextStyle(color: Colors.white54),
-        ),
-        buttonTheme: const ButtonThemeData(
-          buttonColor: Colors.teal,
-        ),
-      ),
-      themeMode: ThemeMode
-          .light, // You can change this to ThemeMode.light or ThemeMode.system
-      home: LoginScreen(),
-    );
-  }
+      theme: AppThemes.lightTheme,
+      darkTheme: AppThemes.darkTheme,
+      initialRoute: Routes.SIGININ,
+      getPages: AppPages.routes,
+    ),
+  );
 }
