4d0bd28c374ee843367917b9b93b9285ebb1def3
ihebmeftah <ihebmeftah666@gmail.com>
2024-06-27 22:39:58 +0100

    Assets

---
+++ lib/constants/assets.dart
@@ -0,0 +1,16 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesEnnaklAutomobiles600RemovebgPreview
+  /// assets/images/ennakl-automobiles--600-removebg-preview.png
+  static const String assetsImagesEnnaklAutomobiles600RemovebgPreview = "assets/images/ennakl-automobiles--600-removebg-preview.png";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+
+  /// Assets for assetsImagesLogoApp
+  /// assets/images/LogoApp.jpg
+  static const String assetsImagesLogoApp = "assets/images/LogoApp.jpg";
+}
+
