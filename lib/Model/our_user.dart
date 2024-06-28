4187c98541ec99432dcaf6252723d1cec1a4bcb2
ihebmeftah <ihebmeftah666@gmail.com>
2024-06-28 11:03:49 +0100

    old work

--- lib/Model/our_user.dart
+++
@@ -1,9 +0,0 @@
-class OurUser {
-  String? uid;
-   String? email;
-   String? password; 
-   String? userName;
-
-
-OurUser({this.email,this.password,this.userName, this.uid});
-}
--- lib/Services/loginservices.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-class LoginServices{
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-}
-
